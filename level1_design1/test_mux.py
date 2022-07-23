c939aedc548dc5201f021a7fcd0a1b862187c4bd
vn218 <vivekm.nair@gmail.com>
2022-07-23 20:56:09 +0000

    modified:   level1_design1/test_mux.py

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -7,16 +7,15 @@ from cocotb.triggers import Timer
 async def test_mux(dut):
     """Test for mux2"""
     for i in range (31):
-        dut.inp0.value = 0
-        globals()[f"dut.inp{i}.value"] == f"{0}"
+        globals()[f"dut.inp{i}.value"] = 0
 
     for i in range(31):
         dut.sel.value = i
-        globals()[f"dut.inp{i}.value"] == f"{2}"
+        globals()[f"dut.inp{i}.value"] = 2
         await Timer(2, units='ns')
         
         assert dut.out.value == 2, f"Adder result is incorrect: {dut.out.value} != 2"
-        globals()[f"dut.inp{i}.value"] == f"{0}"
+        globals()[f"dut.inp{i}.value"] = 0
 
 
     cocotb.log.info('##### CTB: Develop your test here ########')
