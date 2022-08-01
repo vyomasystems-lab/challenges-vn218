47fc41c540b61c6476110a5607be7aab9b5d18c6
vn218 <vivekm.nair@gmail.com>
2022-08-01 07:35:20 +0000

    modified:   level3_design/Makefile 	modified:   level3_design/test_cordicsqrt.py

--- level3_design/test_cordicsqrt.py
+++ level3_design/test_cordicsqrt.py
@@ -7,34 +7,29 @@ from cocotb.clock import Clock
 from cocotb.triggers import RisingEdge, FallingEdge
 
 @cocotb.test()
-async def test_seq_bug1(dut):
+async def test_main(dut):
     """Test for seq detection """
 
     clock = Clock(dut.clk, 10, units="us")  # Create a 10us period clock on port clk
     cocotb.start_soon(clock.start())        # Start the clock
 
-    # reset
-    dut.reset.value = 1
-    await FallingEdge(dut.clk)  
-    dut.reset.value = 0
-    await FallingEdge(dut.clk)
-
     cocotb.log.info('#### CTB: Develop your test here! ######')
 
-    inputs = []
-    for i in range(1000):
+ #   inputs = []
+    for i in range(2**5):
         await FallingEdge(dut.clk)
-        in_bit = random.randint(0,1)
-        dut.inp_bit.value = in_bit
-        inputs.append(in_bit)
-        if (len(inputs) > 9):
-            inputs.pop(0)
+        dut.N.value = i
+ #       inputs.append(in_bit)
+ #       if (len(inputs) > 9):
+ #           inputs.pop(0)
         await RisingEdge(dut.clk)
-        print(inputs)
-        if (len(inputs) > 4):
-            if (inputs[-5:-1] == [1,0,1,1]):
-                assert dut.seq_seen.value == 1, f"Output is incorrect: {dut.seq_seen.value} != 1"
-            else:
-                assert dut.seq_seen.value == 0, f"Output is incorrect: {dut.seq_seen.value} != 0"
-        else:
-                assert dut.seq_seen.value == 0, f"Output is incorrect: {dut.seq_seen.value} != 0"
+        out = dut.sqrt.value
+        print(out.integer)
+ #       print(inputs)
+ #       if (len(inputs) > 4):
+ #           if (inputs[-5:-1] == [1,0,1,1]):
+ #               assert dut.seq_seen.value == 1, f"Output is incorrect: {dut.seq_seen.value} != 1"
+ #           else:
+ #               assert dut.seq_seen.value == 0, f"Output is incorrect: {dut.seq_seen.value} != 0"
+ #       else:
+ #               assert dut.seq_seen.value == 0, f"Output is incorrect: {dut.seq_seen.value} != 0"
